7d98ba2a56408cfbf82c2f8f6f37bb2d2537f7c5
tianyu5177 <zhangtianyu51177@sina.com>
2021-02-05 09:47:07 +0800

    更新

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 export default class App extends Component {
 	render() {
 		return (
 			<div>
 				<h1>我是App组件</h1>
+				<button onClick={this.getStudentInfo}>获取学生的信息</button>
+				{/* <button onClick={this.getCarInfo}>获取汽车的信息</button> */}
 			</div>
 		)
 	}
+
+	getStudentInfo = ()=>{
+		axios.get('http://localhost:3000/students').then(
+			response => {console.log('成功了',response.data)},
+			error => {console.log('失败了',error)}
+		)
+	}
+	// getCarInfo = ()=>{
+	// 	axios.get('http://localhost:3000/cars').then(
+	// 		response => {console.log('成功了',response.data)},
+	// 		error => {console.log('失败了',error)}
+	// 	)
+	// }
 }
